a87e4772e4760ed88d00d4e92da294149a8850d6
Burkay Gül <burkaygul96@gmail.com>
2023-05-09 10:56:00 +0200

    6.gun

--- src/test/java/day06_JUnitFramework/C01_TestNotasyonu.java
+++ src/test/java/day06_JUnitFramework/C01_TestNotasyonu.java
@@ -1,6 +1,39 @@
 package day06_JUnitFramework;
 
+import org.junit.Test;
+
 public class C01_TestNotasyonu {
 
+    /*
+    Bu class icerisinde 3 test calistiralim
+    1- google anasayfaya gidip, url'in google icerdigini test edin
+    2- wisequarter anasayfaya gidip url'in wisequarter icerdigini edin
+    3- amazon anasayfaya gidip, amazon logosunun gorundugunu test edin
+    */
 
+    /*
+        JUnit'in bize sagladigi ilk ve belki de en onemli ozellik
+        test method'larinin bagimsiz olarak calistirilmasini
+        saglayan @Test notasyonudur
+
+
+        JUnit ile class level'dan birden fazla test method'u calistirirsak
+        hangi siralama ile calistiracagini ongoremeyiz ve yonetemeyiz
+     */
+
+    @Test
+    public void googleTest(){
+        System.out.println("google test");
+    }
+
+    @Test
+    public  void wiseTest(){
+        System.out.println("wise Test");
+    }
+
+    @Test
+    public void amazonTest(
+    ){
+        System.out.println("amazon Test");
+    }
 }
